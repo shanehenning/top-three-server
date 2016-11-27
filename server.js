bbc3e57c7fc7f8ff705c05cca68ffc38b33571ea
Shane Henning <shanerhenning@gmail.com>
2016-11-27 15:08:49 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const app = require('express')();
+const mongoose = require('mongoose');
+const Promise = require('./lib/promise');
+const path = require('path');
+const cors = require('cors');
+const httpError = require('http-errors');
+const errorHandler = require('./lib/error-handler');
+mongoose.Promise = Promise;
+
+const serverPort = process.env.PORT || 3000;
+const mongoDatabase = process.env.MONGODB_URI || 'mongodb://localhost/testDB';
+
+mongoose.connect(mongoDatabase);
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/index.html`));
+});
+
+app.all('*', (req, res, next) => {
+  next(httpError(404, 'route not registered'));
+});
+
+app.use(errorHandler);
+
+module.exports = exports = app.listen(serverPort, () => console.log('Server running on ' + serverPort));
