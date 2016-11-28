aecf05e0c01945fd864a20fff1e88e520f63c848
Shane Henning <shanerhenning@gmail.com>
2016-11-27 16:59:25 -0800

    adding in yelp call

--- server.js
+++ server.js
@@ -12,18 +12,21 @@ mongoose.Promise = Promise;
 const serverPort = process.env.PORT || 3000;
 const mongoDatabase = process.env.MONGODB_URI || 'mongodb://localhost/testDB';
 
+const yelpRouter = require('./routes/yelp_router.js');
+
 mongoose.connect(mongoDatabase);
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/index.html`));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(`${__dirname}/index.html`));
+// });
 
 app.all('*', (req, res, next) => {
   next(httpError(404, 'route not registered'));
 });
 
 app.use(errorHandler);
+app.use(yelpRouter);
 
 module.exports = exports = app.listen(serverPort, () => console.log('Server running on ' + serverPort));
