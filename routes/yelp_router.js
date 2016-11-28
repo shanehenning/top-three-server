e21ef1311fb724c7036a63a9a46e62f9d8290bba
Shane Henning <shanerhenning@gmail.com>
2016-11-27 17:25:16 -0800

    routing

--- routes/yelp_router.js
+++ routes/yelp_router.js
@@ -3,52 +3,31 @@
 const Router = require('express').Router;
 // const jsonParser = require('body-parser').json();
 const oauthSignature = require('oauth-signature');
-const yelpRouter = Router();
 
-module.exports = exports = function(){
-  console.log('test');
-  console.log(yelpRouter.get('/', (req, res, next) => {
-    let method = 'GET';
-    let url = 'http://api.yelp.com/v2/search';
-    let params = {
-      location:'Seattle', //req.body.location
-      oauth_consumer_key: 'uRDu5MDX2XJ_WZpwi3DuGA',
-      oauth_token: 'z898hXhCz10qZqGzXATw8ZjATHFuOUuG',
-      oauth_signature_method: 'HMAC-SHA1',
-      oauth_timestamp: new Date().getTime(),
-      oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-      term: 'food', //req.body.term
-    };
-    let consumerSecret = '1-EQsqk2MK10JTYBA4JoPDY87Ik';
-    let tokenSecret = 'F2VqQ2ahPw2LxW8SvmtCBa9CrXY';
-    let signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
-    params['oauth_signaure'] = signature;
-    next();
-  }));
-};
+const yelpRouter = module.exports = exports = Router();
 
-function randomString(length, useableCharacters){
+function randomString(length, useableCharacters) {
   let result = '';
-  for (var i = 0; i < length.length ; i++){
+  for (var i = 0; i < length.length; i++) {
     result += useableCharacters[Math.round(Math.random() * useableCharacters.length - 1)];
   }
   return result;
 }
-// console.log(yelpRouter.post('/', jsonParser, (req, res, next) => {
-//   let method = 'POST';
-//   let url = 'http://api.yelp.com/v2/search';
-//   let params = {
-//     location:'Seattle', //req.body.location
-//     oauth_consumer_key: 'uRDu5MDX2XJ_WZpwi3DuGA',
-//     oauth_token: 'z898hXhCz10qZqGzXATw8ZjATHFuOUuG',
-//     oauth_signature_method: 'HMAC-SHA1',
-//     oauth_timestamp: new Date().getTime(),
-//     oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-//     term: 'food', //req.body.term
-//   };
-//   let consumerSecret = '1-EQsqk2MK10JTYBA4JoPDY87Ik';
-//   let tokenSecret = 'F2VqQ2ahPw2LxW8SvmtCBa9CrXY';
-//   let signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
-//   params['oauth_signaure'] = signature;
-//   next();
-// }));
+yelpRouter.get('/', (req, res, next) => {
+  let method = 'GET';
+  let url = 'http://api.yelp.com/v2/search';
+  let params = {
+    location:'Seattle', //req.body.location
+    oauth_consumer_key: 'uRDu5MDX2XJ_WZpwi3DuGA',
+    oauth_token: 'z898hXhCz10qZqGzXATw8ZjATHFuOUuG',
+    oauth_signature_method: 'HMAC-SHA1',
+    oauth_timestamp: new Date().getTime(),
+    oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+    term: 'food', //req.body.term
+  };
+  let consumerSecret = '1-EQsqk2MK10JTYBA4JoPDY87Ik';
+  let tokenSecret = 'F2VqQ2ahPw2LxW8SvmtCBa9CrXY';
+  let signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
+  params['oauth_signaure'] = signature;
+  next();
+});
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const app = require('express')();
 const mongoose = require('mongoose');
 const Promise = require('./lib/promise');
-const path = require('path');
+// const path = require('path');
 const cors = require('cors');
 const httpError = require('http-errors');
 const errorHandler = require('./lib/error-handler');
@@ -21,7 +21,7 @@ app.use(cors());
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(`${__dirname}/index.html`));
 // });
-app.use(yelpRouter);
+app.use('api/yelp', yelpRouter);
 
 app.all('*', (req, res, next) => {
   next(httpError(404, 'route not registered'));
