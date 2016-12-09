505f2b1b62e3af2614a9ade55cc6873d38979c2c
Shane Henning <shanerhenning@gmail.com>
2016-12-09 01:32:45 -0800

    tryna cors

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ mongoose.connect(mongoDatabase);
 
 app.use(cors());
 
+app.use(function(req,res,next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With', 'Content-Type', 'Accept');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/index.html`));
 });
